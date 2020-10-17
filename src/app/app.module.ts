b2bdc1878f75074c21e505c6c6a45479bae8acac
Brys49 <krzysztof4499@gmail.com>
2020-10-17 14:32:16 +0200

    test merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule } from './calendar/calendar.module';
 import { DatePipe } from '@angular/common';
-
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from '../environments/environment';
-import { CalendarViewMenuComponent } from './calendar-view-menu/calendar-view-menu.component';
+import { CalendarViewMenuComponent } from './calendar/components/calendar-view-menu/calendar-view-menu.component';
 
 
 @NgModule({
--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeekViewComponent } from './views/week-view/week-view.component';
 import { TimePeriodControlerComponent } from './components/time-period-controler/time-period-controler.component';
+import { DayViewComponent } from './views/day-view/day-view.component';
+import { MonthViewComponent } from './views/month-view/month-view.component';
 
 
 @NgModule({
-  declarations: [WeekViewComponent, TimePeriodControlerComponent],
+  declarations: [WeekViewComponent, TimePeriodControlerComponent, DayViewComponent, MonthViewComponent],
   imports: [
     CommonModule
   ],
