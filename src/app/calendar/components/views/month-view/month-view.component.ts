c01eb80504544d47b850aca5bfa433c6b956512d
Brys49 <krzysztof4499@gmail.com>
2020-11-12 20:05:48 +0100

    getting array of days to display on month view

--- src/app/calendar/components/views/month-view/month-view.component.ts
+++ src/app/calendar/components/views/month-view/month-view.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DateHandlerService } from 'src/app/services/date-handler.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+import { EventHandlingService } from '../../../services/event-handling.service';
+import { AddEventDialog } from '../../add-event/add-event-dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-month-view',
   templateUrl: './month-view.component.html',
   styleUrls: ['./month-view.component.scss']
 })
-export class MonthViewComponent implements OnInit {
+export class MonthViewComponent implements OnInit, OnDestroy {
+  daysOfMonth: Date[] = [];
+  daysToDisplay: Date[] = [];
+  today = new Date();
+  dayFormat = 'EEEE';
+  isSmallScreen = false;
+  _subscription: any;
+  subscription: any;
+  rows: number;
 
-  constructor() { }
+  constructor(
+    private dateHandler: DateHandlerService,
+    private breakpointObserver: BreakpointObserver,
+    private eventHandlingService: EventHandlingService,
+    public dialog: MatDialog,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.isSmallScreen = this.breakpointObserver.isMatched('(max-width: 560px)');
+    this.daysOfMonth = this.dateHandler.currentMonth;
+    this._subscription = this.dateHandler.currentMonthChange.subscribe(value => this.daysOfMonth = value);
+    if (this.isSmallScreen) this.dayFormat = 'E';
+    this.fillDaysToDisplay();
+    console.log(this.daysToDisplay);
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
+
+  fillDaysToDisplay(): void {
+    const firstDay: number = this.daysOfMonth[0].getDay() === 0 ? 7 : this.daysOfMonth[0].getDay();
+    //getting days from previous month to fill the first row when the first day of month is not Monday 
+    for (let i = (firstDay - 1); i > 0; i--) {
+      let dayOfPreviousMonth: Date = new Date(this.daysOfMonth[0].getTime());
+      dayOfPreviousMonth.setDate(dayOfPreviousMonth.getDate() - i);
+      this.daysToDisplay.push(dayOfPreviousMonth);
+    }
+    this.daysOfMonth.forEach(element => {
+      this.daysToDisplay.push(element);
+    });
+
+    this.rows = Math.ceil((this.daysToDisplay.length) / 7);
+    const toAdd: number = 7 * this.rows - this.daysToDisplay.length; // number of needed days from the following month
+    //getting days from following month to fill the last row when the last day of month is not Sunday  
+    for (let i = 1; i <= toAdd; i++) {
+      let dayOfFollowingsMonth: Date = new Date(this.daysOfMonth[this.daysOfMonth.length - 1].getTime());
+      dayOfFollowingsMonth.setDate(dayOfFollowingsMonth.getDate() + i);
+      this.daysToDisplay.push(dayOfFollowingsMonth);
+    }
   }
 
 }
