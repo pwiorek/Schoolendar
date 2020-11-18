dda41437e91984ed56bc8c36548287257413ffcf
Piotr <piotr.wiorek71@gmail.com>
2020-11-18 18:54:14 +0100

    Argument of toTypeIconPipe changed from string to TypeEnum

--- src/app/pipes/to-type-icon.pipe.ts
+++ src/app/pipes/to-type-icon.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Type } from '../calendar/components/add-event/typeEnum';
 
 @Pipe({
   name: 'toTypeIcon'
 })
 export class ToTypeIconPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: Type): string {
     switch(value) {
       case     'exam': return 'report';
       case     'quiz': return 'school';
