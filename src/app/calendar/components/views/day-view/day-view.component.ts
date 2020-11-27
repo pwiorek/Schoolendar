d1696930fea8516c8b642990b797759acb073fc6
Brys49 <krzysztof4499@gmail.com>
2020-11-27 23:32:59 +0100

    basic day view

--- src/app/calendar/components/views/day-view/day-view.component.ts
+++ src/app/calendar/components/views/day-view/day-view.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DateHandlerService } from 'src/app/services/date-handler.service';
-import { CalendarViewMenuService } from 'src/app/calendar/services/calendar-view-menu.service';
 import { TimePeriodService } from '../../time-period-controler/time-period.service';
 import { View } from 'src/app/calendar/services/viewEnum';
+import { EventHandlingService } from '../../../services/event-handling.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 
 @Component({
@@ -13,18 +14,22 @@ import { View } from 'src/app/calendar/services/viewEnum';
 })
 export class DayViewComponent implements OnInit, OnDestroy {
   day: Date;
+  hours = this.eventHandlingService.hours;
   private subscription: Subscription;
+  isSmallScreen: boolean;
 
   constructor(
     private dateHandler: DateHandlerService,
-    private calendarViewMenu: CalendarViewMenuService,
-    private timePeriodService: TimePeriodService
+    private timePeriodService: TimePeriodService,
+    private eventHandlingService: EventHandlingService,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
     this.timePeriodService.setView(View.day);
     this.day = this.dateHandler.currentDate;
     this.subscription = this.dateHandler.currentDateChange.subscribe(value => this.day = value);
+    this.isSmallScreen = this.breakpointObserver.isMatched('(max-width: 560px)');
   }
 
   ngOnDestroy() {
